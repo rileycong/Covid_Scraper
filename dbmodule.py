cad9718518e96b1e90418b867cb5d35dd60e398c
Tran Dang <dangcucvip@gmail.com>
2022-03-12 20:59:22 +0700

    change "replace" to "append" in dbmodule.py

--- dbmodule.py
+++ dbmodule.py
@@ -10,10 +10,10 @@ from scrape import cases, deaths, population, recovered
 engine = sqlalchemy.create_engine("mysql+pymysql://lad:1@138.2.88.234:3306/corona_virus_demo", echo=False)
 
 #import cases into database
-cases().to_sql('cases', engine, if_exists='replace', index_label='country_id')
+cases().to_sql('cases', engine, if_exists='append', index_label='country_id')
 
-deaths().to_sql('deaths', engine, if_exists='replace', index_label='country_id')
+deaths().to_sql('deaths', engine, if_exists='append', index_label='country_id')
 
-recovered().to_sql('recovered', engine, if_exists='replace', index_label='country_id')
+recovered().to_sql('recovered', engine, if_exists='append', index_label='country_id')
 
-population().to_sql('population', engine, if_exists='replace', index_label='country_id')
+population().to_sql('population', engine, if_exists='append', index_label='country_id')
