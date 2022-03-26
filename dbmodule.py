e67554d0e2bbe433b7fe89e11c419985fa63900b
Anh Duc Le <gmail@anhducle.com>
2022-03-26 13:44:06 +0000

    Merge branch 'main' of https://github.com/rileycong/Covid_Scraping into main

--- dbmodule.py
+++ dbmodule.py
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
-import os
 from curses import echo
 import os
-=======
-
-from curses import echo
-from dataclasses import replace
->>>>>>> 016b5c5d1fd09dae59d81a938e402f9cfb793e11
 import sqlite3
 import sqlalchemy
 import pandas as pd
 import pymysql
 from scrape import cases, deaths, population, recovered
 
-<<<<<<< HEAD
 cre = os.environ.get('DB_CRE')
 
 engine = sqlalchemy.create_engine(cre, echo=False)
-=======
-engine = sqlalchemy.create_engine("mysql+pymysql://lad:1@138.2.88.234:3306/corona_virus_demo", echo=False)
-
->>>>>>> 016b5c5d1fd09dae59d81a938e402f9cfb793e11
 #import cases into database
 cases().to_sql('cases', engine, if_exists='append', index_label='country_id')
 
